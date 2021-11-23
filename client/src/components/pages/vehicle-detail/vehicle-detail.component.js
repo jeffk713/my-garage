4878e468f0b7dd00b466b5721786956b399c74b4
jeff <jeffk713@gmail.com>
2021-11-23 08:37:13 -0800

    change all indivisual colors to variable with variable scss file. and correct the incorrect file name

--- client/src/components/pages/vehicle-detail/vehicle-detail.component.js
+++ client/src/components/pages/vehicle-detail/vehicle-detail.component.js
@@ -5,7 +5,7 @@ import { createStructuredSelector } from 'reselect';
 
 import ImageDisplay from '../../image-display/image-display.component';
 import IndividualVehicleInfo from '../../individual-vehicle-info/individual-vehicle-info.coponent';
-import ServiceTableHeader from '../../service-table-header/servie-table-header.component';
+import ServiceTableHeader from '../../service-table-header/service-table-header.component';
 import ServiceItemGroup from '../../service-item-group/service-item-group.component';
 import IconButton from '../../icon-button/icon-button.component';
 import CustomButton from '../../custom-button/custom-button.component';
